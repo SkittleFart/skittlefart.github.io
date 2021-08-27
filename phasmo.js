6cf186aa4dd0762ce041e2a8b9562b2cba08cf6f
SkittleFart <mattbamonte@gmail.com>
2021-08-27 02:04:49 -0400

    small update to js

--- phasmo.js
+++ phasmo.js
@@ -8,5 +8,5 @@ fetch("/ghost.json")
         console.log(data[0].name);
         //var $ghostDiv = $("<div>", {id: data[0].name, "class": "ghostBox"});
         var ghostDiv = $("<div id="+data[0].name+" class='ghostBox'><h6>"+data[0].name+"</h6><p>"+data[0].desc+"</p><p>"+data[0].stren+"</p><p>"+data[0].weak+"</p><p>"+data[0].evidence+"</p></div>");
-        $("#ghostList").append($ghostDiv);
+        $("#ghostList").append(ghostDiv);
     });
