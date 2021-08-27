4644649e7b5a9f792f92e00ef11fdcf9bd2611b7
SkittleFart <mattbamonte@gmail.com>
2021-08-27 01:45:02 -0400

    trying something new

--- phasmo.js
+++ phasmo.js
@@ -1,8 +1,17 @@
 var GHOSTS_URL = "ghost.json";
 
+fetch("/ghost.json")
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        console.log(data);
+    });
+/*
 window.onload = function(){
     var mydata = JSON.parse(GHOSTS_URL);
 
     //alert(mydata[0].name);
     console.log(mydata[0].name);
-}; 
+};
+*/
