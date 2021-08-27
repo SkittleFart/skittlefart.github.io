0ed2711df44a7b7463a17e5a0cac4f86e073a1f7
SkittleFart <mattbamonte@gmail.com>
2021-08-27 01:54:16 -0400

    Start of creating ghost div

--- phasmo.js
+++ phasmo.js
@@ -6,12 +6,6 @@ fetch("/ghost.json")
     })
     .then(function(data){
         console.log(data[0].name);
+        var $ghostDiv = $("<div>", {id: data[0].name, "class": "ghostBox"});
+        $("#ghostList").append($ghostDiv);
     });
-/*
-window.onload = function(){
-    var mydata = JSON.parse(GHOSTS_URL);
-
-    //alert(mydata[0].name);
-    console.log(mydata[0].name);
-};
-*/
