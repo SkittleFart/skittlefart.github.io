1dd4b15d5cce9484431338d0a468cd664a9f0873
SkittleFart <mattbamonte@gmail.com>
2021-08-30 00:36:12 -0400

    testing loop

--- phasmo.js
+++ phasmo.js
@@ -1,10 +1,9 @@
-var GHOSTS_URL = "ghost.json";
+var GHOSTS_URL = "/ghost.json";
 
-var testGhosts = '{"name": "Spirit","desc": "A spirit is the most common Ghost you will come across, however it is still very powerful and dangerous. They are usually discovered at one of their hunting grounds after an unexplained death.", "stren": "Nothing", "weak": "Using Smudge Sticks on a Spirit will stop it from attacking for a long period of time.", "evidence": ["EMF 5", "Spirit Box", "Ghost Writing"]}';
+var GHOSTS = {};
+var EVIDENCE = {};
 
-var ghosts = JSON.parse(testGhosts);
-
-fetch("/ghost.json")
+fetch(GHOSTS_URL)
     .then(function(resp){
         return resp.json();
     })
@@ -12,8 +11,10 @@ fetch("/ghost.json")
         //console.log(data[0].name);
 
         for(var i=0; i<data.length; i++){
-            var ghostDiv = $("<div id="+data[i].name+" class='ghostBox'><h6>"+data[i].name+"</h6><p>"+data[i].desc+"</p><p><strong>Strengths: </strong>"+data[i].stren+"</p><p><strong>Weaknesses: </strong>"+data[i].weak+"</p><p class='evidence1'>"+data[i].evidence[0]+"</p><p class='evidence2'>"+data[i].evidence[1]+"</p><p class='evidence3'>"+data[i].evidence[2]+"</p></div>");
+            var ghostDiv = $("<div id="+data[i].name+" class='ghostBox'><h6>"+data[i].name+"</h6><p>"+data[i].desc+"</p><p><strong>Strengths: </strong>"+data[i].stren+"</p><p><strong>Weaknesses: </strong>"+data[i].weak+"</p><p class='evidence'>"+data[i].evidence[0]+"</p><p class='evidence'>"+data[i].evidence[1]+"</p><p class='evidence'>"+data[i].evidence[2]+"</p></div>");
             $("#ghostList").append(ghostDiv);
+            GHOSTS[i] = data[i].name;
+            EVIDENCE[i] = data[i].evidence;
         }
     });
 
@@ -26,9 +27,18 @@ $('.evBTN').on('click', function(evt){
     console.log("testJSON: "+ghosts.evidence[0]);
     console.log("buttonText: "+ $(this).text());
     */
+
     if ($("#ev1 .evBTN").length == 0){ 
         console.log("testing");
         $('#ev1').append(this);
+
+        // scan ghosts to eliminate potential options
+        for(var j=0; j<GHOSTS.length; j++){
+            for(var k=0; k<EVIDENCE[j].length; k++){
+                //if($(this).text() == EVIDENCE[j][k]){}
+                console.log("Evidence["+j+"]["+k+"]: "+EVIDENCE[j][k]);
+            }
+        }
     }else if($("#ev2 .evBTN").length == 0){
         console.log("testing2");
         $('#ev2').append(this);
@@ -36,12 +46,6 @@ $('.evBTN').on('click', function(evt){
         console.log("testing3");
         $('#ev3').append(this);
     }
-    /*
-    var newBTN = $(this);
-    $('#ev1').append(newBTN);
-    */
-    //$('.evBTN').show();
-    //$(this).hide();
 }); 
 
 // Reset button
