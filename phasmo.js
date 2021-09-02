263a771b5bdb785b02fc620a4eb3e7a5c816a263
SkittleFart <mattbamonte@gmail.com>
2021-09-01 20:16:27 -0400

    maybeeeeeeee

--- phasmo.js
+++ phasmo.js
@@ -51,7 +51,7 @@ $('.evBTN').on('click', function(evt){
                     var temp = GHOSTS[j];
                     var temp2 = String("#"+temp+"");
                     console.log(temp2);
-                    $("#Spirit").hide();
+                    $('#'+GHOSTS[j]).hide();
                 }
                 
             }
