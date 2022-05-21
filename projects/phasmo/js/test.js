c9bcd00a30dff17b18756492a06afad952c0d83d
SkittleFart <mattbamonte@gmail.com>
2022-05-21 18:33:56 -0400

    test
    test

--- projects/phasmo/js/test.js
+++ projects/phasmo/js/test.js
@@ -19,18 +19,20 @@ function evidenceDisable(){
 
 function checkEvidenceStatus(evBtnID){
     console.log("checking...");
-    console.log(evBtn.id);
     evBtnID = "#".concat(evBtnID);
     
     if($(evBtnID).hasClass("btn-primary")){
         $(evBtnID).removeClass("btn-primary");
         $(evBtnID).addClass("btn-success");
+        console.log("checking1");
     }else if($(evBtnID).hasClass("btn-success")){
         $(evBtnID).removeClass("btn-success");
         $(evBtnID).addClass("btn-danger");
+        console.log("checking2");
     }else if($(evBtnID).hasClass("btn-danger")){
         $(evBtnID).removeClass("btn-danger");
         $(evBtnID).addClass("btn-primary");
+        console.log("checking3");
     }
 }
 
