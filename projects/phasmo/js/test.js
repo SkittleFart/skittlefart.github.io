f00c335e190fc3166c8b48823ac0d27c8e90a9e4
SkittleFart <mattbamonte@gmail.com>
2022-05-21 18:32:13 -0400

    test
    test

--- projects/phasmo/js/test.js
+++ projects/phasmo/js/test.js
@@ -17,23 +17,24 @@ function evidenceDisable(){
 
 }
 
-function checkEvidenceStatus(evBtn){
+function checkEvidenceStatus(evBtnID){
     console.log("checking...");
     console.log(evBtn.id);
-    /*
-    if($('.evBTN').hasClass("btn-primary")){
-        $('.evBTN').removeClass("btn-primary");
-        $('.evBTN').addClass("btn-success");
-    }else if($('.evBTN').hasClass("btn-success")){
-        $('.evBTN').removeClass("btn-success");
-        $('.evBTN').addClass("btn-danger");
-    }else if($('.evBTN').hasClass("btn-danger")){
-        $('.evBTN').removeClass("btn-danger");
-        $('.evBTN').addClass("btn-primary");
-    }*/
+    evBtnID = "#".concat(evBtnID);
+    
+    if($(evBtnID).hasClass("btn-primary")){
+        $(evBtnID).removeClass("btn-primary");
+        $(evBtnID).addClass("btn-success");
+    }else if($(evBtnID).hasClass("btn-success")){
+        $(evBtnID).removeClass("btn-success");
+        $(evBtnID).addClass("btn-danger");
+    }else if($(evBtnID).hasClass("btn-danger")){
+        $(evBtnID).removeClass("btn-danger");
+        $(evBtnID).addClass("btn-primary");
+    }
 }
 
 $('.evBTN').on('click', function(evt){
     console.log("oh hello there");
-    checkEvidenceStatus(this);
+    checkEvidenceStatus(this.id);
 });
