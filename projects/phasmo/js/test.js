3eff8a781f1eec2bd58f980363921f3630b8e5ec
SkittleFart <mattbamonte@gmail.com>
2023-01-16 17:14:47 -0500

    fds

--- projects/phasmo/js/test.js
+++ projects/phasmo/js/test.js
@@ -8,6 +8,12 @@ var DISABLED_EVIDENCE = [];
 var POSSIBLE_GHOSTS = [];
 var IMPOSSIBLE_GHOSTS = [];
 
+$(document).ready(function() {
+    $('.evBTN').on('click', updateEvidence);
+    $('#resetBTN').on('click', resetEvidence);
+});
+
+
 // fetching ghost info from json and storing in arrays
 fetch(GHOSTS_URL)
     .then(function(resp){
@@ -17,6 +23,8 @@ fetch(GHOSTS_URL)
         //console.log(data[0].name);
 
         for(var i=0; i<data.length; i++){
+            var ghostDiv = $("<div id="+data[i].name+" class='card card col-12 col-md-6 col-lg-4'><div class=card-body><h4 class='card-title'>"+data[i].name+"</h4><div><table class='table table-dark table-bordered'><thead><tr><th value="+removeSpaces(data[i].evidence[0])+">"+data[i].evidence[0]+"</th><th value="+removeSpaces(data[i].evidence[1])+">"+data[i].evidence[1]+"</th><th value="+removeSpaces(data[i].evidence[2])+">"+data[i].evidence[2]+"</th></tr></thead></table></div><div class='card card-body'><p class='card-text'>"+data[i].desc+"</p><p class='card-text'><strong>Strengths:</strong> "+data[i].stren+"</p><p class='card-text'><strong>Weaknesses:</strong> "+data[i].weak+"</p></div></div></div>");
+            $("#ghosts").append(ghostDiv);
             GHOSTS[i] = data[i].name;
             EVIDENCE[i] = data[i].evidence;
         }
