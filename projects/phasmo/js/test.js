e4781945e887f702f99e6e582e084ecab07ee92e
SkittleFart <mattbamonte@gmail.com>
2023-01-25 06:05:28 -0500

    vdfsa

--- projects/phasmo/js/test.js
+++ projects/phasmo/js/test.js
@@ -184,7 +184,7 @@ function updateGhosts(evBtnText){
 
         selectedEvidence.forEach(function(value){
             $("#"+GHOSTS[i]+" th").each(function(){
-                if(compareStrings(value, $(this).text())){
+                if(compareStrings(value, $(this).text()) === 0){
                     console.log("Match! "+value+" / "+$(this).text());
                     evCounter++;
                     isSelectedMatch = true;
@@ -194,7 +194,7 @@ function updateGhosts(evBtnText){
 
         crossedEvidence.forEach(function(value){
             $("#"+GHOSTS[i]+" th").each(function(){
-                if(compareStrings(value, $(this).text())){
+                if(compareStrings(value, $(this).text()) === 0){
                     console.log("Crossed Match! "+value+" / "+$(this).text());
                     crossCounter++;
                     isCrossedMatch = true;
