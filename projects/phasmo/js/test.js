919b33543852993c10adf902219f3cfb404efb6e
SkittleFart <mattbamonte@gmail.com>
2023-01-28 12:54:47 -0500

    fcwsed

--- projects/phasmo/js/test.js
+++ projects/phasmo/js/test.js
@@ -264,7 +264,7 @@ function updateGhosts(evBtnText){
 
     let possibleGhostCount = 0;
 
-    possibleGhostCount = $(".ghostCard[display|=block").length;
+    possibleGhostCount = $(".ghostCard[display|=block]").length;
     console.log("possibleGhostCount: "+possibleGhostCount);
 
     console.log("THIS IS A TEST DO YOU SEE ME");
